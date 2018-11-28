e98b30db013d4333f36ad5da176fbc463ff1dbe3
Adam Birenbaum <abirenbaum721@gmail.com>
2018-11-27 23:41:55 -0600

    Adding Transient

--- server.R
+++ server.R
@@ -6,8 +6,9 @@ server <- function(input,output){
     if (input$sim_type == "Transient"){
       output$ui_transient <- renderUI({
         tagList(
-          textInput("total_time",label = "Total Time"),
-          textInput("time_steps",label = "# Time Steps")
+          textInput("total_time",label = "Total Time",value = 10),
+          textInput("time_step",label = "Time Step",value = 1),
+          textInput("initial_temp",label = "Initial Temp", value = 22)
           
         )
         
@@ -57,7 +58,7 @@ server <- function(input,output){
                                     material,k))
     
     ggplot(df,aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)) + geom_rect(fill = "lightblue",alpha = 0.5,color = "black") + 
-    annotate("text",x =text_df$x, y = text_df$y, label = text_df$label,parse = T) + coord_equal() +theme_void()
+    annotate("text",x =text_df$x, y = text_df$y, label = text_df$label,parse = T) + coord_equal() +theme_void() + labs(title = "Setup") + theme(title = element_text(size = 20))
     
     
     
@@ -140,9 +141,7 @@ server <- function(input,output){
     n <- length(m)
     
     sim_type <- input$sim_type
-    if (sim_type == "Steady State"){
-      
-      
+ 
    
     
     M <- matrix(0,nrow = n, ncol = n)
@@ -216,130 +215,283 @@ server <- function(input,output){
 
     ggplot(df, aes(x = x, y =y, z = temp)) + geom_raster(aes(fill = temp))+geom_contour(color = "black",size = .2) +
       scale_fill_distiller(expression("Temp."~"("*degree*"C)"),palette = "Spectral") + 
-      coord_equal() + theme_minimal() + labs(x = "Width (cm)", y = "Height (cm)") + theme(panel.grid = element_blank())
+      coord_equal() + theme_minimal() + labs(x = "Width (cm)", y = "Height (cm)",title = "Steady State") + theme(panel.grid = element_blank()) +theme(plot.title = element_text(size = 20))
     
-    }else{
-      total_time <- 2
-      dt <- 1
-      n_time_steps <- (total_time / dt + 1)
+    
+    
+  })
+  
+
+  is_error <- function(x) inherits(x,'try-error') || is.na(x)
+  
+  observe({
+    if (input$sim_type == "Transient"){
       
       
-      M <- matrix(0,nrow = n*(n_time_steps - 1), ncol = n*(n_time_steps - 1))
-      B <- matrix(0, nrow = n*(n_time_steps - 1), ncol = 1)
+      n <- input$n_nodes
+      x <- input$dim_x / 100
+      y <- input$dim_y / 100
+      grid_list <- make_initial_grid(x,y,n)
+      m <- grid_list$m
+      n <- length(m)
       
-      initial_temp <- 22
-      
-      
-      alpha <- 9.7E-5
-      tau <- alpha*dt/dx^2
-      
-      for (t in 1:(n_time_steps -1)){
-        for (i in 1:n){
-          print(i)
-          current_neighbors <-  neighbor_m[,i]
-          current_neighbors <- current_neighbors[!is.na(current_neighbors)]
-          neigh_direct <- names(current_neighbors)
+      total_time <- try(as.numeric(input$total_time))
+      dt <- try(as.numeric(input$time_step))
+     initial_temp <- try(as.numeric(input$initial_temp))
+     
+     if (length(total_time) == 0){
+       total_time <- 10
+       dt = 1
+       initial_temp <- 22
+     }
+     
+     
+      if (!(is_error(total_time) || is_error(dt) || is_error(initial_temp))){
+    
+        n_time_steps <- (total_time / dt + 1)
+        total_eqs <- n*(n_time_steps - 1)
+        
+        
+        if (total_eqs < 5000){
+          output$ui_transient_button <- renderUI({
+            tagList(
+              fluidRow(
+                column(width = 6,
+                       tags$button(id = "get_transient", type = "button", class = "btn btn-primary action-button btn-lg", "Calculate Transient")
+                ),
+                column(width = 6,
+                       h4(paste0("# Equations to solve: ", total_eqs))
+                )
+              )
+              
+            )
+            
+          })
           
-          if (is_one_side[i]){
-            
-            missing_direct <- paste0(c("N","E","S","W")[!c("N","E","S","W") %in% neigh_direct],collapse = "")
-            
-            h_total <- switch(missing_direct,"N" = h_top ,"W" =  h_left,"E" = h_right,"S" = h_bottom)
-            b_total <- switch(missing_direct,"N" = h_top * T_top ,"W" =  h_left * T_left,
-                              "E" = h_right * T_right ,"S" = h_bottom * T_bottom )
-            
-            
-            inside_node <- switch(missing_direct,"N" = "S","W" = "E","S" = "N", "E" = "W")
-            
-            
-            
-            # for (j in current_neighbors){
-            #   #M[i,j] <- k*A/2/dx
-            # }
-            row_ind <- (n*(t-1) + i)
-            
-            
-            if (t == 1){
-              B[row_ind,1] <- -2*dx/k* b_total - initial_temp/tau
+        }else{
+          output$ui_transient_button <- renderUI({
+            tagList(
+              fluidRow(
+                column(width = 12,
+                       h4(paste0("Too many equations, will crash system.  Equations to solve: ", total_eqs))
+                )
+              )
               
-            }else{
-              B[row_ind,1] <- -2*dx/k* b_total
-              prev_ind <- (n*(t-2) + i)
-              M[prev_ind,prev_ind] <- 1/tau
-              
-            }
+            )
             
-            M[row_ind,row_ind] <- -(dx*2/k*h_total + 4 + 1/tau)
-            
-            M[row_ind,(n*(t-1) + current_neighbors)] <- ifelse(names(current_neighbors) == inside_node,2,1)
-            
-            
-            
-          }else if (is_corner[i]){
-            
-            missing_direct <- paste0(c("N","E","S","W")[!c("N","E","S","W") %in% neigh_direct],collapse = "")
-            
-            h_total <- switch(missing_direct,"NE" = h_top + h_right,"NW" = h_top + h_left,"ES" = h_right + h_bottom,"SW" = h_bottom + h_left)
-            b_total <- switch(missing_direct,"NE" = h_top * T_top + h_right * T_right,"NW" = h_top * T_top + h_left * T_left,
-                              "ES" = h_right * T_right + h_bottom * T_bottom,"SW" = h_bottom * T_bottom + h_left * T_left)
-            
-            
-            row_ind <- (n*(t-1) + i)
-            
-            
-            if (t == 1){
-              B[row_ind,1] <- -2*dx/k* b_total - initial_temp/tau
-              
-            }else{
-              B[row_ind,1] <- -2*dx/k* b_total
-              prev_ind <- (n*(t-2) + i)
-              M[prev_ind,prev_ind] <- 1/tau
-              
-            }
-            
-            M[row_ind,row_ind] <- -(dx*2/k*h_total + 4 + 1/tau)
-            
-            M[row_ind,(n*(t-1) + current_neighbors)] <- 2
-            
-            
-            
-            
-            
-          }else{
-            
-            row_ind <- (n*(t-1) + i)
-            
-            
-            if (t == 1){
-              B[row_ind,1] <- -initial_temp/tau
-              
-            }
-            
-            M[row_ind,row_ind] <- -(4 + 1/tau)
-            
-            M[row_ind,(n*(t-1) + current_neighbors)] <- 1
-            
-            
-            
-          }
-          
-          
+          })
         }
         
+         
+      }else{
+        output$ui_transient_button <- renderUI({
+      NULL
+        })
       }
       
-      Temps <- solve(M) %*% B
-      
-      df <- data.frame(x = as.numeric(col(m))*dx*100,
-                       y = rev(as.numeric(row(m)))*dx*100,
-                       temp = Temps[,1],
-                       frame = rep(c(1,2),each = n))
+
       
     }
     
   })
   
-  
+  observeEvent(input$get_transient,{
+    
+    output$ui_transient_image <- renderUI({
+      imageOutput("transient",width = "100%")
+      
+      })
+ 
+    
+    
+    x <- input$dim_x / 100
+    y <- input$dim_y / 100
+    n <- input$n_nodes
+    
+    grid_list <- make_initial_grid(x,y,n)
+    m <- grid_list$m
+    dx <- grid_list$dx
+    
+    neighbor_m <- make_neighboring_matrix(m)
+    
+    num_neighbors <- apply(neighbor_m,2,function(x) sum(!is.na(x)))
+    is_corner <- num_neighbors == 2
+    is_one_side <- num_neighbors == 3
+    
+    
+    h_top <- input$h_up
+    h_bottom <- input$h_down
+    h_right <- input$h_right
+    h_left <- input$h_left
+    
+    T_left <- input$T_left
+    T_right <- input$T_right
+    T_top <- input$T_up
+    T_bottom <- input$T_down
+    
+    A = dx
+    As = dx
+    
+    k <- as.numeric(material_df$k[material_df$material == input$material])
+    
+    n <- length(m)
+    
+    
+    total_time <- as.numeric(input$total_time)
+    dt <- as.numeric(input$time_step)
+    n_time_steps <- (total_time / dt + 1)
+    
+    
+    #total_time <- 30
+    #dt <- 1
+    #n_time_steps <- (total_time / dt + 1)
+    
+    
+    
+    M <- matrix(0,nrow = n*(n_time_steps - 1), ncol = n*(n_time_steps - 1))
+    B <- matrix(0, nrow = n*(n_time_steps - 1), ncol = 1)
+    
+    print(dim(M))
+    if (dim(M)[1] > 5000 ) stop("TOO BIG")
+    
+    initial_temp <- as.numeric(input$initial_temp)
+    
+    
+    alpha <- 9.7E-5
+    tau <- alpha*dt/dx^2
+    print(n_time_steps)
+    
+    for (t in 0:(n_time_steps - 2)){
+      for (i in 1:n){
+        
+        current_neighbors <-  neighbor_m[,i]
+        current_neighbors <- current_neighbors[!is.na(current_neighbors)]
+        neigh_direct <- names(current_neighbors)
+        
+        if (is_one_side[i]){
+          
+          missing_direct <- paste0(c("N","E","S","W")[!c("N","E","S","W") %in% neigh_direct],collapse = "")
+          
+          h_total <- switch(missing_direct,"N" = h_top ,"W" =  h_left,"E" = h_right,"S" = h_bottom)
+          b_total <- switch(missing_direct,"N" = h_top * T_top ,"W" =  h_left * T_left,
+                            "E" = h_right * T_right ,"S" = h_bottom * T_bottom )
+          
+          
+          inside_node <- switch(missing_direct,"N" = "S","W" = "E","S" = "N", "E" = "W")
+          
+          
+          
+          # for (j in current_neighbors){
+          #   #M[i,j] <- k*A/2/dx
+          # }
+          row_ind <- (n*(t) + i)
+          
+          
+          if (t == 0){
+            B[row_ind,1] <- -2*dx/k* b_total - initial_temp/tau
+            
+          }else{
+            B[row_ind,1] <- -2*dx/k* b_total
+            prev_ind <- (n*(t-1) + i)
+            M[row_ind,prev_ind] <- 1/tau
+            
+          }
+          
+          M[row_ind,row_ind] <- -(dx*2/k*h_total + 4 + 1/tau)
+          
+          M[row_ind,(n*t + current_neighbors)] <- ifelse(names(current_neighbors) == inside_node,2,1)
+          
+          
+          
+        }else if (is_corner[i]){
+          
+          missing_direct <- paste0(c("N","E","S","W")[!c("N","E","S","W") %in% neigh_direct],collapse = "")
+          
+          h_total <- switch(missing_direct,"NE" = h_top + h_right,"NW" = h_top + h_left,"ES" = h_right + h_bottom,"SW" = h_bottom + h_left)
+          b_total <- switch(missing_direct,"NE" = h_top * T_top + h_right * T_right,"NW" = h_top * T_top + h_left * T_left,
+                            "ES" = h_right * T_right + h_bottom * T_bottom,"SW" = h_bottom * T_bottom + h_left * T_left)
+          
+          
+          row_ind <- (n*t + i)
+          
+          
+          if (t == 0){
+            B[row_ind,1] <- -2*dx/k* b_total - initial_temp/tau
+            
+          }else{
+            B[row_ind,1] <- -2*dx/k* b_total
+            prev_ind <- (n*(t-1) + i)
+            M[row_ind,prev_ind] <- 1/tau
+            
+          }
+          
+          M[row_ind,row_ind] <- -(dx*2/k*h_total + 4 + 1/tau)
+          
+          M[row_ind,(n*t + current_neighbors)] <- 2
+          
+          
+          
+          
+          
+        }else{
+          
+          row_ind <- (n*t + i)
+          
+          
+          if (t == 0){
+            B[row_ind,1] <- -initial_temp/tau
+            
+          }else{
+            prev_ind <- (n*(t-1) + i)
+            M[row_ind,prev_ind] <- 1/tau
+          }
+          
+          M[row_ind,row_ind] <- -(4 + 1/tau)
+          
+          M[row_ind,(n*t + current_neighbors)] <- 1
+          
+          
+          
+        }
+        
+        
+      }
+      
+    }
+    
+    Temps <- solve(M) %*% B
+    
+    df <- data.frame(x = as.numeric(col(m))*dx*100,
+                     y = rev(as.numeric(row(m)))*dx*100,
+                     temp = Temps[,1],
+                     state = round(rep(c(1:(n_time_steps-1)),each = n) * dt,digits = 2))
+
+    g <- ggplot(df, aes(x = x, y =y, z = temp)) + geom_raster(aes(fill = temp),show.legend = F)+    coord_equal() +theme_void()+
+      transition_states(state,transition_length = 1,state_length = 100, wrap = F) + ease_aes('linear') + scale_fill_distiller(palette = "Spectral") + 
+      labs(title = 'Transient - Time: {closest_state} s.') + theme(plot.title = element_text(size = 20))
+    if (n_time_steps < 50){
+      gg <- animate(g,nframes = 100, fps = 20)
+    }else if (n_time_steps < 100){
+      gg <- animate(g,nframes = 200, fps = 25)
+      
+    }else{
+      gg <- animate(g,nframes = 400, fps = 25)
+      
+    }
+   
+    
+    anim_save(paste0(path_to_main,"transient.gif"),animation = gg)
+    output$transient <- renderImage(list(src = paste0(path_to_main,"transient.gif"),contentType = 'image/gif'),deleteFile = T)
+    
+    
+    # geom_contour(color = "black",size = .2) +
+    # scale_fill_distiller(expression("Temp."~"("*degree*"C)"),palette = "Spectral") + 
+    # coord_equal() + theme_minimal() + labs(x = "Width (cm)", y = "Height (cm)") + theme(panel.grid = element_blank()) + 
+    # transition_states(state,transition_length = 1,state_length = 10) + ease_aes('linear')
+    
+    
+    
+  })
   
   
   
--- ui.R
+++ ui.R
@@ -1,98 +1,113 @@
 library(shiny)
 library(ggplot2)
 library(shinydashboard)
-#library(gganimate)
-library(shinyWidgets)
+library(gganimate)
 
 
-material_df <<- read.csv("D:/abire/Documents/heat_transfer_simulations/conduction_material.csv",stringsAsFactors = F)
+
+
+common_path <- "public/project/heat-transfer-sim/"
+if (Sys.info()["nodename"] == "ADAM-DROPLET"){
+  path_to_main <<- paste0("/var/www/adambirenbaum.com/",common_path)
+}else if(Sys.info()["sysname"] == "Windows"){
+  path_to_main <<- "D:/abire/Documents/heat_transfer_sim/"
+}else{
+  path_to_main <<- paste0("~/adambirenbaum.com/",common_path)
+}
+
+
+
+material_df <<- read.csv(paste0(path_to_main,"conduction_material.csv"),stringsAsFactors = F)
 
 header <-  dashboardHeader(
-  title = "Numerical Methods in Heat Transfer"
+  title = "Heat Transfer Simulation on a Rectangular Plate",titleWidth = "100%"
 )
 sidebar <- dashboardSidebar(
-  sidebarMenu(
-    menuItem("Steady-State",tabName = "steady")
-  )
-  
+  # sidebarMenu(
+  #   menuItem("Steady-State",tabName = "steady")
+  # )
+  # 
+  NULL
 )
 
 body <- dashboardBody(
-  tabItems(
-    tabItem(tabName = "steady",
-            fluidRow(
-              column(width = 5,
-                     box(title = NULL,status = "primary",solidHeader = F,width = NULL,
-                         fluidRow(
-                           column(width = 4,
-                                  sliderInput(inputId = "dim_x",label = h4("Width (cm)"),min = 1,max = 100,value = 10),
-                                  sliderInput(inputId = "dim_y",label = h4("Height (cm)"),min = 1,max = 100,value = 10)
-                           ),
-                           column(width = 4,
-                                  sliderInput("n_nodes",h4("# Nodes"), min = 50, max = 1000,value = 500),
-                                  radioButtons("sim_type",h4("Simulation Type"),choices = c("Steady State", "Transient"),
-                                               selected = "Steady State")
-                                  
-                           ),
-                           column(width = 4,
-                                  selectizeInput("material",h4("Material"),choices = material_df$material,selected = "Aluminum"),
-                                  textOutput("material_conduct"),
-                                  uiOutput("ui_transient")
-                                  
-                           )
-                         )
-                         
-                     ),
-                     box(title = NULL,width = NULL,
-                         
-                         
-                         fluidRow(
-                           column(width = 6,
-                                  sliderInput("T_up",h6("Temp. Above"), min = -273, max = 1000, value = 100),
-                                  sliderInput("h_up",h6("Heat Transfer coef. Above"), min = 0, max = 500, value = 20)
-                           ),
-                           column(width = 6,
-                                  sliderInput("T_down",h6("Temp. Below"), min = -273, max = 1000, value = 22),
-                                  sliderInput("h_down",h6("Heat Transfer coef. Below"), min = 0, max = 500, value = 20)
-                           )
-                         ),
-                         hr(),
-                         fluidRow(
-                           column(width = 6,
-                                  sliderInput("T_left",h6("Temp. Left"), min = -273, max = 1000, value = 22),
-                                  sliderInput("h_left",h6("Heat Transfer coef. Left"), min = 0, max = 500, value = 20)
-                           ),
-                           column(width = 6,
-                                  sliderInput("T_right",h6("Temp. Right"), min = -273, max = 1000, value = 22),
-                                  sliderInput("h_right",h6("Heat Transfer coef. Right"), min = 0, max = 500, value = 20)
-                           )
-                           
-                           
-                         )
-                         
-                         
-                         
-                     )
-                     
-                     
-              ),
-              column(width = 7,
-                     plotOutput("steady_state"),
-                     br(),
-
-                     h3("Initial Setup"),
-                     
-                     plotOutput("draw_setup")
-              )
-              
-            )
-            
+  
+  fluidRow(
+    column(width = 5,
+           box(title = NULL,status = "primary",solidHeader = F,width = NULL,
+               fluidRow(
+                 column(width = 4,
+                        sliderInput(inputId = "dim_x",label = h4("Width (cm)"),min = 1,max = 100,value = 10),
+                        sliderInput(inputId = "dim_y",label = h4("Height (cm)"),min = 1,max = 100,value = 10)
+                 ),
+                 column(width = 4,
+                        sliderInput("n_nodes",h4("# Nodes"), min = 20, max = 1000,value = 100),
+                        radioButtons("sim_type",h4("Simulation Type"),choices = c("Steady State", "Transient"),
+                                     selected = "Steady State")
+                        
+                 ),
+                 column(width = 4,
+                        selectizeInput("material",h4("Material"),choices = material_df$material,selected = "Aluminum"),
+                        textOutput("material_conduct"),
+                        uiOutput("ui_transient")
+                        
+                 )
+               )
+               
+           ),
+           
+           uiOutput("ui_transient_button"),
+           
+           box(title = NULL,width = NULL,
+               
+               
+               fluidRow(
+                 column(width = 6,
+                        sliderInput("T_up",h6("Temp. Above"), min = -273, max = 1000, value = 100),
+                        sliderInput("h_up",h6("Heat Transfer coef. Above"), min = 0, max = 500, value = 20)
+                 ),
+                 column(width = 6,
+                        sliderInput("T_down",h6("Temp. Below"), min = -273, max = 1000, value = 22),
+                        sliderInput("h_down",h6("Heat Transfer coef. Below"), min = 0, max = 500, value = 20)
+                 )
+               ),
+               hr(),
+               fluidRow(
+                 column(width = 6,
+                        sliderInput("T_left",h6("Temp. Left"), min = -273, max = 1000, value = 22),
+                        sliderInput("h_left",h6("Heat Transfer coef. Left"), min = 0, max = 500, value = 20)
+                 ),
+                 column(width = 6,
+                        sliderInput("T_right",h6("Temp. Right"), min = -273, max = 1000, value = 22),
+                        sliderInput("h_right",h6("Heat Transfer coef. Right"), min = 0, max = 500, value = 20)
+                 )
+                 
+                 
+               )
+               
+               
+               
+           )
+           
+           
+    ),
+    column(width = 7,
+           plotOutput("steady_state"),
+           
+           
+           uiOutput("ui_transient_image"),
+           
+           
+           
+           plotOutput("draw_setup")
     )
+    
   )
+  
   
 )
 
 
 
 
-dashboardPage(header, sidebar, body)
+dashboardPage(header, dashboardSidebar(disable = T), body)
